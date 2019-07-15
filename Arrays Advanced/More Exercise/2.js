7c9d32f474d93dbed361c25a51d4d7118378c238
banea9 <banea9@gmail.com>
2019-07-15 10:11:54 +0300

    Delete 2.js

--- Arrays Advanced/More Exercise/2.js
+++
@@ -1,9 +0,0 @@
-function solve(num1,num2,num3) {
-    function sum (num1,num2) {
-        return num1 + num2;
-    }
-    function subtract(num3) {
-        return num3;
-    }
-    console.log(sum(num1, num2) - subtract(num3))
-}
